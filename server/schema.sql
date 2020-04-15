5ef7b537e3f78801b457c823b79c5d4388716d09
serviolee <serviolee@gmail.com>
2020-04-15 16:28:51 -0700

    working on creating schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,10 +1,32 @@
+-- mysql -u root < server/schema.sql
+DROP DATABASE chat;
 CREATE DATABASE chat;
 
 USE chat;
 
+DROP TABLE IF EXISTS messages;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS rooms;
+
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id INT(11),
+  text VARCHAR(100),
+  createdAt DATETIME,
+  PRIMARY KEY (id)
 );
+
+CREATE TABLE users (
+  id INT(11),
+  username VARCHAR(100),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE rooms (
+  id INT(11),
+  roomname VARCHAR(100),
+  PRIMARY KEY (id)
+);
+
 
 /* Create other tables and define schemas for them here! */
 
