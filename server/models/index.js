22395b6e9b7e49c70518cfdacfcc34588481b598
serviolee <serviolee@gmail.com>
2020-04-16 08:24:48 -0700

    implemented get all messages from db

--- server/models/index.js
+++ server/models/index.js
@@ -2,7 +2,23 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
+    get: function (callback) {
+      db.connection.query('select m.text, m.createdAt, r.roomname, u.username from messages m inner join rooms r on (m.room_id = r.id) inner join users u on (m.user_id = u.id)', function (err, results, fields) {
+        if (err) {
+          console.log(err);
+        } else {
+          callback(null, results);
+        }
+      });
+    },
+    // a function which produces all the messages
+    // add callback param
+    // query and select all from messages, join rooms name and users name
+    // callback: anon func that takes in err results and fields
+    //// results will contain the results of the query
+    //// fields will contain info about the returning results
+    //// if (err) => console.log(err) else callback(null, results)
+
     post: function () {} // a function which can be used to insert a message into the database
   },
 
