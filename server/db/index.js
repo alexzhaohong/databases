843d26189b3dd443c6f367523b6e0187149c18d7
serviolee <serviolee@gmail.com>
2020-04-15 17:41:47 -0700

    Update server db index

--- server/db/index.js
+++ server/db/index.js
@@ -12,6 +12,18 @@ var connection = mysql.createConnection({
   database: 'chat'
 });
 
-connection.connect();
+connection.connect((err) => {
+  if (err) {
+    console.log('error connecting to database: ', err);
+    return;
+  } else {
+    console.log('connected to database! With id: ' + connection.threadId);
+  }
+});
+
+connection.query('SELECT 1', function (error, results, fields) {
+  if (error) throw error;
+  // connected!
+});
 
 exports.connection = connection;
