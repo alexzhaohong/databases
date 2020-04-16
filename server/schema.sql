952316ccb4c52be2033fc7c7d7ede1bf61dac4be
serviolee <serviolee@gmail.com>
2020-04-15 17:19:07 -0700

    updated schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,5 +1,5 @@
 -- mysql -u root < server/schema.sql
-DROP DATABASE chat;
+DROP DATABASE IF EXISTS chat;
 CREATE DATABASE chat;
 
 USE chat;
@@ -9,24 +9,32 @@ DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS rooms;
 
 CREATE TABLE messages (
-  id INT(11),
+  id INT(11) NOT NULL AUTO_INCREMENT,
   text VARCHAR(100),
   createdAt DATETIME,
+  user_id INT(11),
+  room_id INT(11),
   PRIMARY KEY (id)
 );
 
 CREATE TABLE users (
-  id INT(11),
+  id INT(11) NOT NULL AUTO_INCREMENT,
   username VARCHAR(100),
   PRIMARY KEY (id)
 );
 
 CREATE TABLE rooms (
-  id INT(11),
+  id INT(11) NOT NULL AUTO_INCREMENT,
   roomname VARCHAR(100),
   PRIMARY KEY (id)
 );
 
+-- ---
+-- Foreign Keys
+-- ---
+
+ALTER TABLE messages ADD FOREIGN KEY (user_id) REFERENCES users (id);
+ALTER TABLE messages ADD FOREIGN KEY (room_id) REFERENCES rooms (id);
 
 /* Create other tables and define schemas for them here! */
 
